44d556179436685f28c09289ed120e3db490c3af
unknown <mullrai@D02DI1146441.net1.cec.eu.int>
2014-06-18 10:13:36 +0200

    enlever pour le moment la possibilité d'assimiler un dépôt à un autre dépôt

--- github2spip_pipelines.php
+++ github2spip_pipelines.php
@@ -35,13 +35,13 @@ function github2spip_affiche_milieu($flux) {
 
 
 	// github_depots sur les github_depots
-	if (!$e['edition'] AND in_array($e['type'], array('github_depot'))) {
+	/*if (!$e['edition'] AND in_array($e['type'], array('github_depot'))) {
 		$texte .= recuperer_fond('prive/objets/editer/liens', array(
 			'table_source' => 'github_depots',
 			'objet' => $e['type'],
 			'id_objet' => $flux['args'][$e['id_table_objet']]
 		));
-	}
+	}*/
 	// github_repos sur les github_depots
 	if (!$e['edition'] AND in_array($e['type'], array('github_depot'))) {
 		$texte .= recuperer_fond('prive/objets/editer/liens', array(
