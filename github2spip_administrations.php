2151197d914a03552db057f39c63dbd25b0003ec
abelass <rainer@websimple.be>
2018-04-09 13:12:42 +0200

    des testes

--- github2spip_administrations.php
+++ github2spip_administrations.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Fichier gérant l'installation et désinstallation du plugin Gestionnaire de dépots GitHub 
+ * Fichier gérant l'installation et désinstallation du plugin Gestionnaire de dépots GitHub
  *
- * @plugin     Gestionnaire de dépots GitHub 
+ * @plugin     Gestionnaire de dépots GitHub
  * @copyright  2018
  * @author     Rainer Müller
  * @licence    GNU/GPL
@@ -22,7 +22,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * - créer la structure SQL,
  * - insérer du pre-contenu,
  * - installer des valeurs de configuration,
- * - mettre à jour la structure SQL 
+ * - mettre à jour la structure SQL
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
@@ -32,26 +32,12 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 **/
 function github2spip_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# 
-	# $maj['create'] = array(array('creer_base'));
-	#
-	# include_spip('inc/config')
-	# $maj['create'] = array(
-	#	array('maj_tables', array('spip_xx', 'spip_xx_liens')),
-	#	array('ecrire_config', 'github2spip', array('exemple' => "Texte de l'exemple"))
-	#);
-	#
-	# $maj['1.1.0']  = array(array('sql_alter','TABLE spip_xx RENAME TO spip_yy'));
-	# $maj['1.2.0']  = array(array('sql_alter','TABLE spip_xx DROP COLUMN id_auteur'));
-	# $maj['1.3.0']  = array(
-	#	array('sql_alter','TABLE spip_xx CHANGE numero numero int(11) default 0 NOT NULL'),
-	#	array('sql_alter','TABLE spip_xx CHANGE texte petit_texte mediumtext NOT NULL default \'\''),
-	# );
-	# ...
+
 
 	$maj['create'] = array(array('maj_tables', array('spip_github_depots', 'spip_github_depots_liens', 'spip_github_repos', 'spip_github_repos_liens')));
+	$maj['1.0.1'] = array(array('maj_tables', array( 'spip_github_repos')));
+
+
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -60,11 +46,11 @@ function github2spip_upgrade($nom_meta_base_version, $version_cible) {
 
 /**
  * Fonction de désinstallation du plugin Gestionnaire de dépots GitHub .
- * 
+ *
  * Vous devez :
  *
  * - nettoyer toutes les données ajoutées par le plugin et son utilisation
- * - supprimer les tables et les champs créés par le plugin. 
+ * - supprimer les tables et les champs créés par le plugin.
  *
  * @param string $nom_meta_base_version
  *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
